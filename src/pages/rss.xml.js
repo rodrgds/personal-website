06d68b46087baf573a5b0769a3c434269e7c09c8
Rodrigo Dias <kraktoos@gmail.com>
2025-08-03 11:10:03 +0100

    chore: update dependencies and improve RSS feed generation
    - Updated various Astro-related dependencies to their latest versions for improved performance and features.
    - Changed the export function in rss.xml.js from 'get' to 'GET' to align with the new conventions.
    - Enhanced the RSS feed by including post titles and publication dates in the feed items.

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,13 +5,15 @@ import sanitizeHtml from 'sanitize-html';
 import MarkdownIt from 'markdown-it';
 const parser = new MarkdownIt();
 
-export async function get(context) {
+export async function GET(context) {
 	const blog = await getCollection('blog');
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
 		items: blog.map((post) => ({
+			title: post.data.title,
+			pubDate: post.data.pubDate,
 			link: `/posts/${post.slug}/`,
 			content: sanitizeHtml(parser.render(post.body)),
 			...post.data,
