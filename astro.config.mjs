c4f37105b92a25c8d87d341d509412382d0e48f8
Rodrigo Dias <kraktoos@gmail.com>
2025-12-30 20:12:08 +0000

    refactor: Reorganize component structure, introduce new UI components and a tool layout, update dependencies, and remove Tailwind CSS configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
-import purgecss from "astro-purgecss";
+// import purgecss from "astro-purgecss";
 import compress from "astro-compress";
 
 export default defineConfig({
   site: "https://rgo.pt",
-  integrations: [mdx(), sitemap(), svelte(), purgecss(), compress()],
+  integrations: [mdx(), sitemap(), svelte(), compress()],
   markdown: {
     syntaxHighlight: "prism",
   },
