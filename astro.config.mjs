e039f77e3d4f2789ea93d921fe72562ed3044be1
Rodrigo Dias <kraktoos@gmail.com>
2026-03-02 20:09:27 +0000

    chore: add @astrojs/vercel dependency and update vlc logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,16 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
 // import purgecss from "astro-purgecss";
 import compress from "astro-compress";
 
 export default defineConfig({
   site: "https://rgo.pt",
-  output: "static",
-  adapter: node({ mode: "standalone" }),
+  output: "server",
+  adapter: vercel(),
   integrations: [mdx(), sitemap(), svelte(), compress()],
   markdown: {
     syntaxHighlight: "prism",
   },
-  // output: "hybrid",
 });
